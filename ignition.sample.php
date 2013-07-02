d87ed7a00619b743e889ac197fe2a6d3dded1eb4
Jacob Roeland <jacob@jacroe.com>
2013-07-02 14:19:01 -0500

    Add ability for modules to add content to header/footer, e.g. Javascript

--- ignition.sample.php
+++ ignition.sample.php
@@ -8,7 +8,20 @@ define("IGN_TIMEZONE", "America/Chicago");
 define("IGN_URL", "http://localhost/ignition/");
 define("IGN_PATH", "/home/jacob/www/ignition/");
 
-foreach (glob(IGN_PATH."modules/*.php") as $includes) require_once($includes);
+foreach (glob(IGN_PATH."modules/*.php") as $includes)
+{
+	$module = str_replace(IGN_PATH."modules/", "", str_replace(".php", "", $includes));
+	require_once(IGN_PATH."modules/$module.php");
+
+	if(function_exists("ign_{$module}_header"))
+	{
+		$header .= call_user_func("ign_{$module}_header");
+	}
+	if(function_exists("ign_{$module}_footer"))
+	{
+		$footer .= call_user_func("ign_{$module}_footer");
+	}
+}
 require_once(IGN_PATH."lib/smarty/Smarty.class.php");
 $smarty = new Smarty;
 $smarty->left_delimiter = '{{';
