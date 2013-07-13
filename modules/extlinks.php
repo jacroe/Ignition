60f5d6945a2e09045822c0315cd6bb35fa79ecf2
Jacob Roeland <jacob@jacroe.com>
2013-07-12 23:22:21 -0500

    Added more extlinks #1

--- modules/extlinks.php
+++ modules/extlinks.php
@@ -3,7 +3,28 @@ function ign_extlinks_footer()
 {
 	return <<<RETURN
 <script type="text/javascript">
-	$('a').each(function() { var link = $(this).attr("href"); if(link.match("youtube.com")) { $(this).addClass("extlink ext-youtube"); $(this).attr("target", "_BLANK"); } } )
+	$('a').each(function()
+	{
+		var link = $(this).attr("href");
+		if(link.match("youtube.com"))
+		{
+			$(this).addClass("extlink ext-youtube");
+			$(this).attr("target", "_BLANK");
+		}
+		if(link.match("wikipedia.org"))
+		{
+			$(this).addClass("extlink ext-wikipedia");
+			$(this).attr("target", "_BLANK");
+		}
+		if(link.match("twitter.com"))
+		{
+			$(this).addClass("extlink ext-twitter");
+			$(this).attr("target", "_BLANK");
+		}
+
+		if(link.match(".pdf"))
+			$(this).addClass("filelink file-pdf");
+	});
 </script>
 
 RETURN;
