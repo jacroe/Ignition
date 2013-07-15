7ad8d6102387fbbed16e3f573ceefd6d12ea7c48
Jacob Roeland <jacob@jacroe.com>
2013-07-15 17:05:21 -0500

    Cleanup - Remove a tab a line - It's sans-serif, not sans according to Sublime Text

--- modules/extlinks.php
+++ modules/extlinks.php
@@ -3,33 +3,33 @@ function ign_extlinks_footer()
 {
 	return <<<RETURN
 <script type="text/javascript">
-	$('a').each(function()
+$('a').each(function()
+{
+	var link = $(this).attr("href");
+	if(link.match("youtube.com"))
 	{
-		var link = $(this).attr("href");
-		if(link.match("youtube.com"))
-		{
-			$(this).addClass("extlink ext-youtube");
-			$(this).attr("target", "_BLANK");
-		}
-		if(link.match("wikipedia.org"))
-		{
-			$(this).addClass("extlink ext-wikipedia");
-			$(this).attr("target", "_BLANK");
-		}
-		if(link.match("twitter.com"))
-		{
-			$(this).addClass("extlink ext-twitter");
-			$(this).attr("target", "_BLANK");
-		}
-		if(link.match("wolframalpha.com"))
-		{
-			$(this).addClass("extlink ext-wolfram");
-			$(this).attr("target", "_BLANK");
-		}
+		$(this).addClass("extlink ext-youtube");
+		$(this).attr("target", "_BLANK");
+	}
+	if(link.match("wikipedia.org"))
+	{
+		$(this).addClass("extlink ext-wikipedia");
+		$(this).attr("target", "_BLANK");
+	}
+	if(link.match("twitter.com"))
+	{
+		$(this).addClass("extlink ext-twitter");
+		$(this).attr("target", "_BLANK");
+	}
+	if(link.match("wolframalpha.com"))
+	{
+		$(this).addClass("extlink ext-wolfram");
+		$(this).attr("target", "_BLANK");
+	}
 
-		if(link.match(".pdf"))
-			$(this).addClass("filelink file-pdf");
-	});
+	if(link.match(".pdf"))
+		$(this).addClass("filelink file-pdf");
+});
 </script>
 
 RETURN;
