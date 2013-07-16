82fe2256c6548c2a39a18604ec8d5ec5aad0755b
Jacob Roeland <jacob@jacroe.com>
2013-07-16 15:23:50 -0500

    New file structure - For more than one post per day

--- modules/update.php
+++ modules/update.php
@@ -9,7 +9,7 @@ foreach (array_reverse(glob(IGN_PATH."data/drafts/*.json")) as $post)
 		$jsonPost["date"] = date("r", strtotime($jsonPost["publish"]));
 		unset($jsonPost["publish"]);
 		file_put_contents($post, json_format(json_encode($jsonPost)));
-		$newSlug = date("Y-m-d_", strtotime($jsonPost["date"])).$slug;
+		$newSlug = date("Y-m-d-Hi_", strtotime($jsonPost["date"])).$slug;
 		rename(IGN_PATH."data/drafts/$slug.json", IGN_PATH."data/posts/$newSlug.json");
 		ign_email_send(USER_NAME, USER_EMAIL, "{$jsonPost['title']} has been posted.", "We just posted \"{$jsonPost['title']}\". You can read it <a href=\"".IGN_URL."?id=$newSlug\"> here</a>.");
 	}
