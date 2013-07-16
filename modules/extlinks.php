ee8a5be593674d6397a72d06fc66609facbd44d6
Jacob Roeland <jacob@jacroe.com>
2013-07-16 15:23:20 -0500

    External links open in new window

--- modules/extlinks.php
+++ modules/extlinks.php
@@ -3,32 +3,32 @@ function ign_extlinks_footer()
 {
 	return <<<RETURN
 <script type="text/javascript">
+// Selecting external links thanks to
+// http://jquery-howto.blogspot.com/2009/06/find-select-all-external-links-with.html
+$.expr[':'].external = function(obj)
+{
+	return !obj.href.match(/^mailto\:/) && (obj.hostname != location.hostname);
+};
+
 $('a').each(function()
 {
 	var link = $(this).attr("href");
 	if(link.match("youtube.com"))
-	{
 		$(this).addClass("extlink ext-youtube");
-		$(this).attr("target", "_BLANK");
-	}
+
 	if(link.match("wikipedia.org"))
-	{
 		$(this).addClass("extlink ext-wikipedia");
-		$(this).attr("target", "_BLANK");
-	}
+
 	if(link.match("twitter.com"))
-	{
 		$(this).addClass("extlink ext-twitter");
-		$(this).attr("target", "_BLANK");
-	}
+
 	if(link.match("wolframalpha.com"))
-	{
 		$(this).addClass("extlink ext-wolfram");
-		$(this).attr("target", "_BLANK");
-	}
 
 	if(link.match(".pdf"))
 		$(this).addClass("filelink file-pdf");
+
+	$('a:external').attr("target", "_BLANK");
 });
 </script>
 
