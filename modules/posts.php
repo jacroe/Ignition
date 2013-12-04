8e4ba2f198d40dcdeada4b8631c9fb4ca1212786
Jacob Roeland <jacob@jacroe.com>
2013-12-03 23:14:49 -0600

    Added some support for footnotes  - Can use Dokuwiki's style of ((footnotes))  - It's not as nice as PHP Markdown Extra (which I've since learned is actually Free Software under the BSD license :unamused: )  - This isn't a permanent solution. I may update this in the future. But it worsk for now.

--- modules/posts.php
+++ modules/posts.php
@@ -36,9 +36,38 @@ function ign_posts_getBySlug($slug)
 		return false;
 }
 
+function ign_posts_parsePost($article)
+{
+	$article = Parsedown::instance()->parse($article);
+
+	/**************************************************************
+	 * Converts footnotes from a text to those
+	 * similar to PHP Markdown Extra
+	 * Gleefully stolen and modified from Michelle Steigerwalt
+	 * http://msteigerwalt.com/articles/2008/08/17/Footnotes
+	 **************************************************************/
+	$pattern = "/\(\((.*?)\)\)/i"; # Footnote pattern: ((<message>))
+	preg_match_all($pattern, $article, $matches);
+	$notes = Array();
+	$i = 1;
+	foreach ($matches[0] as $footnote)
+	{
+		$notes[$i] = $matches[1][$i-1];
+		$newText = "<sup id=\"fnref-$i\"><a href=\"#fn-$i\">$i</a></sup>";
+		$article = str_replace($footnote, $newText, $article);
+		$i++;
+	}
+	foreach ($notes as $i=>$note)
+		$footnotes .= '<li id="fn-'.$i.'"><p>'.$note.' <a href="#fnref-'.$i.'">&#8617;</a></p></li>'."\n";
+	if ($footnotes)
+		$article .= "\n<hr />\n<ol>\n$footnotes</ol>";
+
+	return $article;
+}
+
 function ign_posts_getPostData($jsonPost, $slug)
 {
-	return array("title"=>$jsonPost->title, "author"=>$jsonPost->author, "slug"=>$slug, "date"=>$jsonPost->date, "html5Date"=>ign_html5Time($jsonPost->date), "timeAgo"=>ign_timeAgo($jsonPost->date), "loc"=>$jsonPost->loc, "excerpt"=>$jsonPost->excerpt, "article"=>Parsedown::instance()->parse($jsonPost->article), "type"=>$jsonPost->type, "tags"=>$jsonPost->tags, "link"=>$jsonPost->link, "photo"=>$jsonPost->photo);
+	return array("title"=>$jsonPost->title, "author"=>$jsonPost->author, "slug"=>$slug, "date"=>$jsonPost->date, "html5Date"=>ign_html5Time($jsonPost->date), "timeAgo"=>ign_timeAgo($jsonPost->date), "loc"=>$jsonPost->loc, "excerpt"=>$jsonPost->excerpt, "article"=>ign_posts_parsePost($jsonPost->article), "rawArticle"=>$jsonPost->article, "type"=>$jsonPost->type, "tags"=>$jsonPost->tags, "link"=>$jsonPost->link, "photo"=>$jsonPost->photo);
 }
 
 function ign_posts_publish($title, $author, $publishDate, $loc, $excerpt, $article, $type = "post", $tags = "", $link = "", $photo = "")
